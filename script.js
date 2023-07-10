8a9ca36b660f2df28b3c3411d35b4f35f3f2a1f8
Luiz Neto <luiznetopb@gmail.com>
2023-07-10 00:58:55 -0300

    Deixando o site responsivo

--- script.js
+++ script.js
@@ -6,45 +6,45 @@ const selectedCurrency = document.getElementById('currency');
 const result = document.getElementById('result');
 let valueConverted = 0;
 
-function handleSubmit(e) {
-  e.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
 
-  if(!inputValue.value || inputValue.value < 0) {
-    alert('Informe um valor correto!');
-    return;
-  } else if(!selectedCurrency.value) {
-    alert('Escolha uma moeda!');
-    return;
-  }
+        if (!inputValue.value || inputValue.value < 0) {
+            alert('Informe um valor correto!');
+            return;
+        } else if (!selectedCurrency.value) {
+            alert('Escolha uma moeda!');
+            return;
+        }
 
-  converter();
-}
+        converter();
+    }
 
-function converter() {
-  if(selectedCurrency.value === 'eur') {
-    valueConverted = inputValue.value * 6.35;
-    result.innerHTML = valueFormatter('pt-PT', 'EUR');
+    function converter() {
+        if (selectedCurrency.value === 'eur') {
+            valueConverted = inputValue.value * 6.35;
+            result.innerHTML = valueFormatter('pt-PT', 'EUR');
 
-    animateResult();
-  } else if(selectedCurrency.value === 'dol') {
-    valueConverted = inputValue.value * 5.61;
-    result.innerHTML = valueFormatter('en-US', 'USD');
+            animateResult();
+        } else if (selectedCurrency.value === 'dol') {
+            valueConverted = inputValue.value * 5.61;
+            result.innerHTML = valueFormatter('en-US', 'USD');
 
-    animateResult();
-  }
+            animateResult();
+        }
 
-  inputValue.value = '';
-  selectedCurrency.value = '';
-}
+        inputValue.value = '';
+        selectedCurrency.value = '';
+    }
 
-function valueFormatter(locale, currency) {
-  const value = valueConverted.toLocaleString(`${locale}`, { style: 'currency', currency: `${currency}` });
-  return `<span>💵</span> ${value} <span>💵</span>`;
-}
+    function valueFormatter(locale, currency) {
+        const value = valueConverted.toLocaleString(`${locale}`, {style: 'currency', currency: `${currency}`});
+        return `<span>💵</span> ${value} <span>💵</span>`;
+    }
 
-function animateResult() {
-  return result.animate([
-    { transform: 'translateY(-150px)'},
-    { transform: 'translateY(0px)'}
-  ], { duration: 500 });
-}
+    function animateResult() {
+        return result.animate([
+            {transform: 'translateY(-150px)'},
+            {transform: 'translateY(0px)'}
+        ], {duration: 500});
+    }
